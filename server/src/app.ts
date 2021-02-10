22036de4c7f9345a7a6a45be9eaca6c497e806ca
mpiorowski <mateuszpiorowski@gmail.com>
2021-02-10 01:23:02 +0100

    db connected

---
+++ server/src/app.ts
@@ -0,0 +1,31 @@
+require("dotenv").config();
+import bodyParser from "body-parser";
+import compression from "compression";
+import cors from "cors";
+import express from "express";
+import helmet from "helmet";
+import morganBody from "morgan-body";
+import { usersRouter } from "./usersRouter";
+
+const app = express();
+app.use(compression());
+app.use(helmet());
+app.use(express.json());
+app.use(cors());
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+morganBody(app);
+
+// routes
+app.use(usersRouter);
+
+// define a route handler for the default home page
+app.get("/", (req, res) => {
+  res.send("welcome home");
+});
+
+// start the Express server
+app.listen(process.env.SERVER_PORT, () => {
+  console.log("server started at http://localhost:" + process.env.SERVER_PORT);
+});
---
+++ server/src/usersRouter.ts
@@ -0,0 +1,52 @@
+import express from "express";
+import { Pool, QueryResult } from "pg";
+
+type Users = {
+  id: string;
+  name: string;
+  email: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export const usersRouter = express.Router().use(express.json());
+
+usersRouter.get("/api/users", async (req, res) => {
+  const pool = new Pool();
+  const client = await pool.connect();
+  let response: QueryResult<Users>;
+  try {
+    await client.query("BEGIN");
+    response = await client.query(`select * from users`);
+    await client.query("COMMIT");
+    return res.send(JSON.stringify(response.rows[0]));
+  } catch (e) {
+    await client.query("ROLLBACK");
+    console.log(e);
+    return res.status(500).send({
+      message: "Something went wrong",
+    });
+  } finally {
+    client.release();
+  }
+});
+
+usersRouter.post("/api/users", async (req, res) => {
+  const pool = new Pool();
+  const client = await pool.connect();
+  let response: QueryResult<Users>;
+  try {
+    await client.query("BEGIN");
+    response = await client.query(`insert into users (name, email) values ('${req.body.name}', '${req.body.email}') returning *`);
+    await client.query("COMMIT");
+    return res.send(JSON.stringify(response.rows[0]));
+  } catch (e) {
+    await client.query("ROLLBACK");
+    console.log(e);
+    return res.status(500).send({
+      message: "Something went wrong",
+    });
+  } finally {
+    client.release();
+  }
+});
